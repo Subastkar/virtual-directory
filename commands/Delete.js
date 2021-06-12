255f1725e033235d55f28db71e710f2b52adf42a
Alberto Alcaraz <jaalcarazz@yahoo.com>
2021-06-12 14:48:14 -0500

    Added delete method

---
+++ commands/Delete.js
@@ -0,0 +1,39 @@
+const Command = require("./Command");
+
+class Create extends Command {
+  /**
+   *  Will delete a folder given an specific path
+   */
+  static do(directory, args) {
+    if (args.length === 0) {
+      console.log('MOVE command needs one arguments.');
+      return false;
+    }
+
+    const path = args[0].split('/');
+    const dirLength = path.length;
+    let pastFolder = directory;
+    let folderName = 'root';
+
+    for (let count = 0; count < dirLength; count++) {
+      folderName = path[count];
+
+      if (!pastFolder) {
+        console.log(`Directory path invalid.`);
+        break;
+      } else if (folderName === '') {
+        console.log(`Directory name invalid.`);
+        break;
+      };
+
+      if (count === dirLength - 1) {
+        delete pastFolder[folderName];
+        break;
+      }
+
+      pastFolder = pastFolder[folderName];
+    }
+  }
+}
+
+module.exports = Create;
