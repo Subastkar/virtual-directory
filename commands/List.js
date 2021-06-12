1280d9b7ba72b708cbf576b3dfe56ba4545e624b
Alberto Alcaraz <jaalcarazz@yahoo.com>
2021-06-12 16:26:11 -0500

    Updated list command to a mover visual output

--- commands/List.js
+++ commands/List.js
@@ -6,8 +6,26 @@ class List extends Command {
    *
    */
   static do(directory) {
-    console.log(directory);
+    console.log('\nDirectory status -----------------');
+    this.iterate(directory);
     return true;
+  }
+
+  static iterate(directory, deep = 0) {
+    Object.keys(directory).forEach((rootKey) => {
+      const item = directory[rootKey];
+
+      this.print(deep, rootKey);
+
+      if (typeof item === 'object') {
+        return this.iterate(item, deep + 1);
+      }
+    });
+  }
+
+  static print(deep, string) {
+    const prefix = '.'.repeat(deep);
+    console.log(`${prefix}${string}`);
   }
 }
 
